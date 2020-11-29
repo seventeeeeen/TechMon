a1c671d34ffe311370d0c3e2c99bfde532a32fb7
juna <ju0830na17@gmail.com>
2020-11-29 15:34:00 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 河本珠奈 on 2020/11/29.
+//  Copyright © 2020 Kawamoto Juna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
